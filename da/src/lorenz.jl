552178ee91878ccb05abf905c2522c4958fca4d3
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-06-09 23:18:26 +0100

    added basic 2d fem

--- da/src/lorenz.jl
+++ da/src/lorenz.jl
@@ -7,8 +7,17 @@ lorenz = @ode_def begin
 end σ ρ β
 
 u0 = [1.0, 1.0, 1.0]
+# tspan = (0.0, 10000.0)
 tspan = (0.0, 100.0)
 p = [10.0, 28.0, 8 / 3]
+
+condition(u, t, integrator) = u[1]
+effect!(integrator) = nothing
+
+cb = ContinuousCallback(condition, effect!, save_positions = (true, false))
+
 prob = ODEProblem(lorenz, u0, tspan, p)
-sol = solve(prob)
+sol = solve(prob, Tsit5())
 plot(sol, vars = (1, 2, 3), legend = :none)
+# sol_pc = solve(prob, Vern9(), callback = cb, save_everystep = false)
+# scatter(sol_pc, vars = (2, 3), legend = :none)
